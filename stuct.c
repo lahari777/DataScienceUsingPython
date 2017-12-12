e5c4251a1b21daf9fd6c97d0649a82b9830028ec
lahari777 <34370893+lahari777@users.noreply.github.com>
2017-12-12 16:57:07 +0530

    Add files via upload

---
+++ stuct.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{
+struct student
+{
+int rollno;
+char name[80];
+char sec;
+};
+struct student s1;
+printf("\n enter the rollnumber");
+scanf("%d",&s1.rollno");
+printf("\n enter the name");
+scanf("%s",&s1.name);
+printf("\n enter the sec");
+scanf("%s",&s1.sec);
+printf("\n ******student details******");
+printf("\n rollno=%d",s1.rollno);
+printf("\n name=%s",s1.name);
+printf("n sec=%s",s1.sec);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+5%%s
